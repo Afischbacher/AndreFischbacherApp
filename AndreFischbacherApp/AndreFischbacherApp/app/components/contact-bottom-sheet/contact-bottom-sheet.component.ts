401426eee8da640cbb4a1ffd5ce7966e3ef3553e
Afischbacher <ital-andre95@live.com>
2020-10-02 17:16:17 -0400

    added linked in as contact method

--- AndreFischbacherApp/AndreFischbacherApp/app/components/contact-bottom-sheet/contact-bottom-sheet.component.ts
+++ AndreFischbacherApp/AndreFischbacherApp/app/components/contact-bottom-sheet/contact-bottom-sheet.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import { MatBottomSheetRef } from '@angular/material/bottom-sheet';
-import { faTwitter, faGithub, faSkype, IconDefinition } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter, faGithub, faSkype, IconDefinition, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 @Component({
     selector: "contact-bottom-sheet",
     templateUrl: "./contact-bottom-sheet.component.html",
@@ -11,6 +11,7 @@ export class ContactBottomSheet {
     faTwitter : IconDefinition = faTwitter;
     faGithub : IconDefinition  = faGithub;
     faSkype : IconDefinition  = faSkype;
+    faLinkedIn : IconDefinition = faLinkedin;
     constructor(private _bottomSheetRef: MatBottomSheetRef<ContactBottomSheet>) {
         
     }
