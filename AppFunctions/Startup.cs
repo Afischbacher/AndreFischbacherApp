bcc573272f77406dd569ae41c8f02af288606ed1
ANDRE-PC\ital- <ital-andre95@live.com>
2021-07-01 13:43:42 -0400

    comments

--- AppFunctions/Startup.cs
+++ AppFunctions/Startup.cs
@@ -29,18 +29,25 @@ namespace AndreFischbacherApp.Functions
 
 			var connectionString = configurationRoot["AndreFischbacherApp:Database:ConnectionString"] ?? configurationRoot["DatabaseConnectionString"];
 
+			// Add DbContext and SQL connection
 			builder.Services.AddDbContext<IAndreFischbacherAppContext, AndreFischbacherAppContext>
 				(options => options.UseSqlServer(connectionString));
 
 			// Register Mediator
 			builder.Services.AddMediatR(typeof(IMediatorDataContextBase));
 
+			// Http Client
 			builder.Services.AddSingleton(new HttpClient());
 
+			// App Settings
 			builder.Services.AddScoped<IAppConfiguration, AppConfiguration>();
+
+			// Repositories
 			builder.Services.AddScoped<IInterestsContentRepository, InterestsContentRepository>();
 			builder.Services.AddScoped<IAboutMeContentRepository, AboutMeContentRepository>();
 			builder.Services.AddScoped<ICareerContentRepository, CareerContentRepository>();
+
+			// Services
 			builder.Services.AddScoped<IFunctionWarmingService, FunctionWarmingService>();
 
 		}
