a9cdb933e7a4c155d36474b2c2e364993c815fd9
ANDRE-PC\ital- <ital-andre95@live.com>
2021-12-27 23:12:51 -0500

    updated timer trigger

--- AppFunctions/v1/Timer/FunctionWarmingTimerTriggerFunction.cs
+++ AppFunctions/v1/Timer/FunctionWarmingTimerTriggerFunction.cs
@@ -8,10 +8,8 @@ namespace AndreFischbacherApp.Functions.v1.Timer
 {
 	public class FunctionWarmingTimerTriggerFunction
 	{
-		private readonly IMediator _mediator;
-
 		[FunctionName("FunctionWarmingTimerTriggerFunction")]
-		public async Task Run([TimerTrigger("0 */2 * * * *", RunOnStartup = true)] TimerInfo myTimer, ILogger log)
+		public async Task Run([TimerTrigger("0 */15 * * * *", RunOnStartup = true)] TimerInfo myTimer, ILogger log)
 		{
 		}
 	}
