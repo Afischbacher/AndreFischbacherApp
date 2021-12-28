7d1e3f23b6a4be4a6229ecbd0f3f86f0fdf60432
ANDRE-PC\ital- <ital-andre95@live.com>
2021-12-27 23:12:37 -0500

    updated to angular 13 and updated timer trigger

--- AndreFischbacherApp/AndreFischbacherApp/polyfills.ts
+++ AndreFischbacherApp/AndreFischbacherApp/polyfills.ts
@@ -14,22 +14,8 @@
  * Learn more in https://angular.io/guide/browser-support
  */
 
-/***************************************************************************************************
-* BROWSER POLYFILLS
-*/
-
-
-/** IE10 and IE11 requires the following for NgClass support on SVG elements */
- import 'classlist.js';  // Run `npm install --save classlist.js`.
-
-/** IE10 and IE11 requires the following for the Reflect API. */
- import 'core-js/es6/reflect';
-
-
 /** Evergreen browsers require these. **/
 // Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
-
-
 
 /**
  * Web Animations `@angular/platform-browser/animations`
@@ -57,8 +43,6 @@
  * Zone JS is required by default for Angular itself.
  */
  import 'zone.js';  // Included with Angular CLI.
-
-
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
