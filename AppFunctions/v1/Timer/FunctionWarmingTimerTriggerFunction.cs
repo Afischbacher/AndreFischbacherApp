5c5a8ad572810852d57081acdb30cb9ec3304548
ANDRE-PC\ital- <ital-andre95@live.com>
2022-01-29 14:16:30 -0500

    disabled trigger

--- AppFunctions/v1/Timer/FunctionWarmingTimerTriggerFunction.cs
+++ AppFunctions/v1/Timer/FunctionWarmingTimerTriggerFunction.cs
@@ -6,6 +6,7 @@ namespace AndreFischbacherApp.Functions.v1.Timer
 {
     public class FunctionWarmingTimerTriggerFunction
 	{
+		[Disable]
 		[FunctionName("FunctionWarmingTimerTriggerFunction")]
 		public void Run([TimerTrigger("0 */10 * * * *", RunOnStartup = true)] TimerInfo myTimer, ILogger log)
 		{
